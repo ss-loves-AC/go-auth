82dffa3d8d544041b4ed95029c3abe9a4a1cb13f
ss-loves-AC <ssompu88@gmail.com>
2025-02-02 16:46:32 +0530

    implemented redis cache and mysql db interface for user and tokens

--- storage/cache.go
+++ storage/cache.go
@@ -1,20 +1,48 @@
 package storage
 
-import "go-auth/models"
+import (
+	"context"
+	"go-auth/models"
+	"log"
+
+	"github.com/go-redis/redis/v8"
+)
 
 type Cache interface {
-	Get(key string) (*models.User, bool)
-	Set(key string, value *models.User)
+
+	GetUser(ctx context.Context, key string) (*models.User, bool)    // Retrieve user data by key
+	SetUser(ctx context.Context, key string, value *models.User)    // Store user data
+
+	GetRefreshToken(ctx context.Context, jti string) (*models.RefreshToken, bool) // Retrieve refresh token by JTI
+	SetRefreshToken(ctx context.Context, jti string, value *models.RefreshToken)   // Store refresh token
+
+	GetRevokedToken(ctx context.Context, jti string) (*models.RevokedToken, bool) // Retrieve revoked token by JTI
+	SetRevokedToken(ctx context.Context, jti string, value *models.RevokedToken)   // Store revoked token
 }
 
-type RedisCache struct{}
+type RedisCache struct {
+	Client *redis.Client
+}
 
 func NewCache() (*RedisCache, error) {
-	return &RedisCache{}, nil
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+		DB:   0,
+	})
+
+	return &RedisCache{
+		Client: client,
+	}, nil
 }
 
-func (r *RedisCache) Get(key string) (*models.User, bool) {
-	return nil, false
-}
+func (r *RedisCache) GetUser(ctx context.Context, key string) (*models.User, bool)
 
-func (r *RedisCache) Set(key string, value *models.User) {}
+func (r *RedisCache) SetUser(ctx context.Context, key string, value *models.User)
+
+func (r *RedisCache) GetRefreshToken(ctx context.Context, jti string) (*models.RefreshToken, bool)
+
+func (r *RedisCache) SetRefreshToken(ctx context.Context, jti string, value *models.RefreshToken)
+
+func (r *RedisCache) GetRevokedToken(ctx context.Context, jti string) (*models.RevokedToken, bool)
+
+func (r *RedisCache) SetRevokedToken(ctx context.Context, jti string, value *models.RevokedToken)
--- storage/database.go
+++ storage/database.go
@@ -2,9 +2,20 @@ package storage
 
 import (
 	"context"
+	"fmt"
 	"go-auth/models"
+	"log"
 
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+)
+
+const (
+	dbHost     = "mysql"
+	dbName     = "go_auth"
+	dbUser     = "go_user"
+	dbPassword = "go_password"
+	dbPort     = "3306"
 )
 
 type DB interface {
@@ -20,3 +31,41 @@ type DB interface {
 type MysqlDB struct {
 	DB *gorm.DB
 }
+
+func NewDB() (*MysqlDB, error) {
+
+	source := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(source), &gorm.Config{})
+	if err != nil {
+		log.Println("gorm Db connection ", err)
+		return nil, fmt.Errorf("gorm DB connection error: %w", err)
+	}
+
+	tables := []interface{}{
+		&models.User{},
+		&models.RefreshToken{},
+		&models.RevokedToken{},
+	}
+
+	if err := db.AutoMigrate(tables...); err != nil {
+		fmt.Println("Failed to auto migrate:", err)
+		return nil, fmt.Errorf("failed to auto migrate: %w", err)
+
+	}
+
+	return &MysqlDB{DB: db}, nil
+}
+
+
+func (db *MysqlDB) GetUser(ctx context.Context, email string) (*models.User, error)
+
+func (db *MysqlDB) CreateUser(ctx context.Context, user *models.User) (*models.User, error)
+
+func (db *MysqlDB) GetToken(ctx context.Context, jti string) (*models.RefreshToken, error)
+
+func (db *MysqlDB) CreateToken(ctx context.Context, token *models.RefreshToken) (*models.RefreshToken, error)
+
+func (db *MysqlDB) RevokeToken(ctx context.Context, jti string) error
+
+func (db *MysqlDB) RefreshToken(ctx context.Context, jti string) (*models.RefreshToken, error)
