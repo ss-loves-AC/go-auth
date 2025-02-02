cd281b4876946d549dbbecc566aeac476074eff1
ss-loves-AC <ssompu88@gmail.com>
2025-02-03 05:09:38 +0530

    implemented APIs in the router package

--- router/router.go
+++ router/router.go
@@ -1,1 +1,20 @@
 package router
+
+import (
+	"go-auth/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter(authHandler *handler.AuthHandler, tokenHandler *handler.TokenHandler) *gin.Engine {
+	r := gin.Default()
+
+	r.POST("/signup", authHandler.SignUp)
+	r.POST("/signin", authHandler.SignIn)
+
+	r.POST("/authorize", tokenHandler.Authorize)
+	r.POST("/revoke", tokenHandler.RevokeToken)
+	r.POST("/refresh", tokenHandler.RefreshToken)
+
+	return r
+}
